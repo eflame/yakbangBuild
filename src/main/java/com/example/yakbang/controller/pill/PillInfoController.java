d3c7522f67b15af18245817257550296bb43ccb9
EunSilKO <n6fg49@gmail.com>
2024-08-13 11:03:18 +0900

    pill detail, list 데이터 적용

--- src/main/java/com/example/yakbang/controller/pill/PillInfoController.java
+++ src/main/java/com/example/yakbang/controller/pill/PillInfoController.java
@@ -1,15 +1,28 @@
 package com.example.yakbang.controller.pill;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import com.example.yakbang.dto.pill.PillDTO;
+import com.example.yakbang.service.pill.PillService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/pill")
+@RequiredArgsConstructor
 public class PillInfoController {
 
+    private final PillService pillService;
+
     @GetMapping("/detail")
-    public String pillDetail (){
+    public String pillDetail(@RequestParam("id") Long id, Model model) {
+        // 서비스에서 id를 통해 약품 상세 정보 조회
+        PillDTO pill = pillService.getPillById(id);
+        model.addAttribute("pill", pill);
+
+        // 상세 페이지 템플릿 반환
         return "pill/pill_detail";
     }
 
@@ -18,4 +31,12 @@ public class PillInfoController {
         return "pill/pill_list";
     }
 
+    @GetMapping
+    public String getPills(Model model) {
+        List<PillDTO> pills = pillService.getPillsInfo();
+        model.addAttribute("pills", pills);
+        return "pill/pill_list";
+    }
+
 }
+
--- src/main/java/com/example/yakbang/mapper/pill/PillMapper.java
+++ src/main/java/com/example/yakbang/mapper/pill/PillMapper.java
@@ -3,14 +3,20 @@ package com.example.yakbang.mapper.pill;
 import com.example.yakbang.dto.pill.PillDTO;
 import com.example.yakbang.dto.pill.PillItemDTO;
 import com.example.yakbang.dto.pill.PillOtcDTO;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface PillMapper {
     void insertPill(PillItemDTO pillItemDTO);
 
     void updatePill(PillOtcDTO pillOtcDTO);
+
+    List<PillDTO> findAll();
+
+    @Select("SELECT * FROM TBL_PILL WHERE ITEM_SEQ = #{id}")
+    PillDTO findById(@Param("id") Long id);
 
 }
 
