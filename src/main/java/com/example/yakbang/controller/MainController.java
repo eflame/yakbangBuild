9b41d0e92fdf7077daf1e1d369743ad4381247ff
oos98 <samwoll25@naver.com>
2024-08-18 23:08:29 +0900

    리뷰 수정, 리뷰 목록 메인에 추가

--- src/main/java/com/example/yakbang/controller/MainController.java
+++ src/main/java/com/example/yakbang/controller/MainController.java
@@ -2,7 +2,9 @@ package com.example.yakbang.controller;
 
 import com.example.yakbang.dto.board.BoardQnaListDTO;
 import com.example.yakbang.dto.board.BoardSearchDTO;
+import com.example.yakbang.dto.board.ReviewListDTO;
 import com.example.yakbang.service.board.BoardService;
+import com.example.yakbang.service.board.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +17,21 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MainController {
     private final BoardService boardService;
+    private final ReviewService reviewService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(String keyword, Model model) {
         List<BoardQnaListDTO> list = boardService.findList(); // qna 게시물 목록 호출
         List<BoardQnaListDTO> limitedList = list.stream()     // qna 게시물 목록 4가지로 제한
                 .limit(4)
                 .collect(Collectors.toList());
         model.addAttribute("list", limitedList);
+
+        List<ReviewListDTO> list2 = reviewService.findList(keyword);
+        List<ReviewListDTO> limitedList2 = list2.stream()     // review 게시물 목록 8가지로 제한
+                .limit(8)
+                .toList();
+        model.addAttribute("list2", limitedList2);
 
         return "main";
     }
