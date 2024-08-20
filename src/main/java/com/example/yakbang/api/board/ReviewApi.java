aa18f3a7847734d70bbf2879980ef0e67b867d4b
refactoringK <refactoring.k@gmail.com>
2024-08-20 17:51:20 +0900

    기능추가(review/list): 백엔드 작업 완료

--- src/main/java/com/example/yakbang/api/board/ReviewApi.java
+++ src/main/java/com/example/yakbang/api/board/ReviewApi.java
@@ -1,13 +1,18 @@
 package com.example.yakbang.api.board;
 
+import com.example.yakbang.dto.board.ReviewListDTO;
 import com.example.yakbang.dto.board.ReviewPillSearchDTO;
+import com.example.yakbang.dto.pill.PageRequest;
 import com.example.yakbang.service.board.ReviewService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ReviewApi {
@@ -16,6 +21,14 @@ public class ReviewApi {
     @GetMapping("/v1/pills/names")
     public List<ReviewPillSearchDTO> getPillList(String pillName){
         return reviewService.findPillListByName(pillName);
+    }
+
+    @GetMapping("/v1/reviews")
+    public List<ReviewListDTO> getReviewList(String keyword, PageRequest pageRequest){
+//        log.info("keyword = {}, pageRequest = {}", keyword, pageRequest);
+        List<ReviewListDTO> listWithPage = reviewService.findListWithPage(keyword, pageRequest);
+//        log.info("listWithPage = {}", listWithPage);
+        return listWithPage;
     }
 }
 
--- src/main/java/com/example/yakbang/mapper/board/ReviewMapper.java
+++ src/main/java/com/example/yakbang/mapper/board/ReviewMapper.java
@@ -4,7 +4,9 @@ import com.example.yakbang.dto.board.ReviewDetailListDTO;
 import com.example.yakbang.dto.board.ReviewListDTO;
 import com.example.yakbang.dto.board.ReviewPillSearchDTO;
 import com.example.yakbang.dto.board.ReviewWriteDTO;
+import com.example.yakbang.dto.pill.PageRequest;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,6 +15,9 @@ public interface ReviewMapper {
     void insertReview(ReviewWriteDTO reviewWriteDTO);
 
     List<ReviewListDTO> selectList(String keyword);
+
+    List<ReviewListDTO> selectListWithPage(@Param("keyword") String keyword,
+                                           @Param("pageRequest") PageRequest pageRequest);
 
     List<ReviewDetailListDTO> selectDetailList(Long pillId);
 
